9602563ab01018e4ead689a18ba279c276b3bb5c
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-05 15:58:42 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assignment_hungry_ninja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
